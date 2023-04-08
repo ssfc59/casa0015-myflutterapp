e5d38b997d058c2ad6f0a822b59174a7088d2d74
Sophia Chong <zcrhsfc@ucl.ac.uk>
2023-04-08 22:53:27 +0100

    added views

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,114 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("Today's Sunset Forecast",
-                   style: TextStyle(
-                fontSize: 36.0,
-                fontWeight: FontWeight.bold,
-                color: HexColor("#6675FF"),
-              ),),
-        backgroundColor: Colors.indigoAccent,
-      ),
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomLeft,
-               stops: [
-                0.1,
-                0.5,
-                0.99,
-                ],
-            colors: [
-              HexColor("#6675FF"),
-              HexColor("#E7936F"),
-              HexColor("#FEAD34"),
-            ],
-          )),
-          child:
-          Center(
-
-              child: CircleAvatar(
-            backgroundColor: HexColor("FF710B"),
-            radius: 110,
-          )
-          ), //child center, circle avatar
-        ), //child container
-      ), //body center
-    ));
+      title: 'Sunset Time App',
+      home: const FirstRoute(),
+      routes: {
+        "second": (context) {
+          return const SecondRoute();
+        },
+        "third": (context) {
+          return const ThirdRoute();
+        }
+      },
+    );
   }
 }
 
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Today's Sunset Forecast",
+              style: GoogleFonts.workSans( 
+                 textStyle: TextStyle(
+                fontSize: 30.0,
+                color: HexColor("fddec5"),
+              )
+              ) 
+              ),
+          backgroundColor: HexColor("8a94f9"),
+        ),
+        body: GestureDetector(
+            child: Center(
+              child: Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomLeft,
+                    stops: const [
+                      0.1,
+                      0.5,
+                      0.99,
+                    ],
+                    colors: [
+                      HexColor("8a94f9"),
+                      HexColor("eaae99"),
+                      HexColor("febb5b"),
+                    ],
+                  )),
+                  child: Center(
+                    child: Container(
+                        child: CircleAvatar(
+                            backgroundColor: HexColor("fd9f6f"),
+                            radius: 110,
+                            child: Center(
+                                child: Text('6:59 PM',
+                                    style: GoogleFonts.nabla(
+                                      textStyle: TextStyle(
+                                        fontSize: 50.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: HexColor("ffffff"),
+                                      ),
+                                    ))))),
+                  )),
+            ),
+            onTap: () {
+              Navigator.pushNamed(context, 'second');
+            }));
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Second Route"),
+        ),
+        body: GestureDetector(
+            child: const Center(child: Text('Second Route')),
+            onTap: () {
+              Navigator.pushNamed(context, 'third');
+            }));
+  }
+}
+
+class ThirdRoute extends StatelessWidget {
+  const ThirdRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Third Route"),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:s/main.dart';
+import 'package:s/main.dart' show MyApp;
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
