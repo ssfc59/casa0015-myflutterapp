1954671dee544c535aad4bb13f4fb4b4daa66caf
Sophia Chong <zcrhsfc@ucl.ac.uk>
2023-04-21 11:54:50 +0100

    Stack and column widgets

--- lib/main.dart
+++ lib/main.dart
@@ -174,9 +174,130 @@ class ThirdRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Third Route"),
+      return Scaffold(
+                appBar: AppBar(
+          title: Text("Sunset 3",
+              style: GoogleFonts.workSans(
+                  textStyle: TextStyle(
+                fontSize: 30.0,
+                color: HexColor("fddec5"),
+              ))),
+          backgroundColor: HexColor("8a94f9"),
+        ),
+      body: Stack(
+        children: [
+          Container(
+              constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomLeft,
+                    stops: const [
+                      0.1,
+                      0.5,
+                      0.99,
+                    ],
+                    colors: [
+                      HexColor("8a94f9"),
+                      HexColor("eaae99"),
+                      HexColor("febb5b"),
+                    ],
+                  )
+            ),
+
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  children: [ 
+                    const SizedBox(height: 30.0),
+                    AspectRatio(
+                      aspectRatio: 6,
+                      child: Container(
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          //color: Colors.white.withOpacity(.4),
+                          //borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Center(
+                          child: Text("Today's sunset forecast",
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 24.0)),
+                        ),
+                      ),
+                    ),
+
+
+
+                    AspectRatio(
+                      aspectRatio: 1.2,
+                      child: Container(
+                              child: Center(
+                    child: Card(
+                        elevation: 20.0,
+                        //shadowColor: HexColor("ffd837"),
+                        shadowColor: Color.fromARGB(255, 255, 94, 0),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(110.0)),
+                        child: SizedBox(
+                          width: 220.0,
+                          height: 220.0,
+                          child: FittedBox(
+                            child: FloatingActionButton(
+                              onPressed: () {},
+
+                              child: Container(                              
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomLeft,
+                                    colors: [
+                                      HexColor("fd9f6f"),
+                                      HexColor("fea729")
+                                    ],
+                                  ),
+                                ),
+                                child: Center( 
+                                  child: Text('6:59 PM',
+                                    style: GoogleFonts.nabla(
+                                      textStyle: TextStyle(
+                                        fontSize: 12.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: HexColor("ffffff"),
+                                      ),
+                                    )),
+                                  )
+                              ),
+                            
+                            ),
+                          ),
+                        )),
+                  )
+                      ),
+                    ),
+
+                    const SizedBox(height: 20.0),
+                    AspectRatio(
+                      aspectRatio: 10,
+                      child: Container(
+                        //padding: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                          //color: Colors.white.withOpacity(.4),
+                          //borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Center(
+                          child: Text('Quality: 78%',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 24.0)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            
+          ),
+        ],
       ),
     );
   }
