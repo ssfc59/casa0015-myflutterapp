6837284e1f6dcea8af9b3f964ada430552c9589f
Sophia Chong <zcrhsfc@ucl.ac.uk>
2023-04-24 14:59:20 +0100

    created main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'dart:convert'; 
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sunset Time App',
       home: MyWidget(),
+      
     );
   }
 }
@@ -42,65 +43,161 @@ class _MyWidgetState extends State<MyWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Today's Sunset Forecast"),
+        title: Text("Sunset Sunrise Times",
+            style: GoogleFonts.workSans(
+                textStyle: TextStyle(
+              fontSize: 25.0,
+              color: HexColor("ffffff"), //fddec5
+            ))),
+        backgroundColor: HexColor("8a94f9"),
       ),
-      body: FutureBuilder<Map<String, dynamic>>(
-        future: getData(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            String sunset = snapshot.data!['results']['sunset'];
-            String firstFourChars = sunset.substring(0, 4);
-            String sunsetWithPM = '$firstFourChars PM';
-
-            return Center(
-                child: SizedBox(
-                    width: 220.0,
-                    height: 220.0,
-                    child: FittedBox(
-
-
-                      child: FloatingActionButton(
-                        onPressed: () {
-                          print(sunsetWithPM);
-                        },
-                        child: Container(
-                          width: 220.0,
-                          height: 220.0,
-                       decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            gradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomLeft,
-                              colors: [HexColor("fd9f6f"), HexColor("fea729")],
-                            ),
-                          ),
-child: Center(
-                          child: Text(
-                            sunsetWithPM,
-                            style: GoogleFonts.nabla(
-                              textStyle: TextStyle(
-                                fontSize: 12.0,
-                                fontWeight: FontWeight.bold,
-                                color: HexColor("ffffff"),
+      body: Stack(
+        children: [
+          Container(
+              child: GestureDetector(
+                  child: Center(
+                      child: Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomLeft,
+                      stops: const [
+                        0.1,
+                        0.5,
+                        0.99,
+                      ],
+                      colors: [
+                        HexColor("8a94f9"),
+                        HexColor("eaae99"),
+                        HexColor("febb5b"),
+                      ],
+                    )),
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 50.0),
+                          AspectRatio(
+                            aspectRatio: 4,
+                            child: Container(
+                              padding: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                  //color: Colors.white.withOpacity(.4),
+                                  //borderRadius: BorderRadius.circular(15),
+                                  ),
+                              child: Center(
+                                child: Text("Today's sunset forecast:",
+                                    style: GoogleFonts.workSans(
+                                        textStyle: TextStyle(
+                                      fontSize: 24.0,
+                                      color: HexColor("ffffff"), //fddec5
+                                    ))),
                               ),
                             ),
                           ),
-)
-                        ),
+                          AspectRatio(
+                            aspectRatio: 1.3,
+                            child: FutureBuilder<Map<String, dynamic>>(
+                              future: getData(),
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  String sunset =
+                                      snapshot.data!['results']['sunset'];
+                                  String firstFourChars =
+                                      sunset.substring(0, 4);
+                                  String sunsetWithPM = '$firstFourChars PM';
+
+                                  return Center(
+                                      child: SizedBox(
+                                          width: 220.0,
+                                          height: 220.0,
+                                          child: FittedBox(
+                                            child: FloatingActionButton(
+                                              onPressed: () {
+                                                print(sunsetWithPM);
+                                              },
+                                              child: Container(
+                                                  width: 220.0,
+                                                  height: 220.0,
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    gradient: LinearGradient(
+                                                      begin: Alignment.topLeft,
+                                                      end: Alignment.bottomLeft,
+                                                      colors: [
+                                                        HexColor("fd9f6f"),
+                                                        HexColor("fea729")
+                                                      ],
+                                                    ),
+                                                  ),
+                                                  child: Center(
+                                                    child: Text(
+                                                      sunsetWithPM,
+                                                      style: GoogleFonts.nabla(
+                                                        textStyle: TextStyle(
+                                                          fontSize: 12.0,
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                          color: HexColor(
+                                                              "ffffff"),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  )),
+                                            ),
+                                          )));
+                                } else if (snapshot.hasError) {
+                                  return Center(
+                                    child: Text('Error: ${snapshot.error}'),
+                                  );
+                                } else {
+                                  return Center(
+                                    child: CircularProgressIndicator(),
+                                  );
+                                }
+                              },
+                            ),
+                          ), //futurebuilder
+                          const SizedBox(height: 20.0),
+                          AspectRatio(
+                            aspectRatio: 10,
+                            child: Container(
+                              //padding: EdgeInsets.all(5.0),
+                              decoration: BoxDecoration(),
+                              child: Center(
+                                child: Text('Quality: 78%',
+                                     style: GoogleFonts.workSans(
+                                        textStyle: TextStyle(
+                                      fontSize: 24.0,
+                                      color: HexColor("ffffff"), //fddec5
+                                    ))),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20.0),
+                          AspectRatio(
+                            aspectRatio: 10,
+                            child: Container(
+                              //padding: EdgeInsets.all(5.0),
+                              decoration: BoxDecoration(),
+                              child: Center(
+                                child: Text('Great Quality!',
+                                  style: GoogleFonts.workSans(
+                                        textStyle: TextStyle(
+                                      fontSize: 24.0,
+                                      color: HexColor("ffffff"), //fddec5
+                                    ))),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-
-
-                    )));
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+                    ),
+                  )),
+                  onTap: () {
+                    Navigator.pushNamed(context, 'second');
+                  })),
+        ],
       ),
     );
   }
